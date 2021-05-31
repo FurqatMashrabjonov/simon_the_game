d80d78bd08ac117f4d1414aae4a88e3d6307a97c
FurqatMashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2021-05-31 12:35:42 +0500

    simon
    simon the game

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import axios from 'vue-axios'
+import VueAxios from 'vue-axios'
+
+import SignUp from "@/views/SignUp";
+import Main from "@/views/Main";
+import Login from "@/views/Login";
+
+
+// require('./assets/css/swipe.min.css')
+// require('./assets/css/lib/bootstrap.min.css')
+// require('./assets/img/favicon.png')
+
+
+Vue.use(VueAxios, axios)
+
+const routes = [
+    { path: '/signup', component: SignUp },
+    { path: '/home', component: Main },
+    { path: '/login', component: Login }
+]
+
+
+const router = new VueRouter
+({
+    mode: 'history',
+    routes
+})
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+    router: router,
+}).$mount('#app')
---
+++ src/views/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Signup</h1>
+</template>
+
+<script>
+export default {
+  name: "SignUp"
+}
+</script>
+
+<style scoped>
+
+</style>
